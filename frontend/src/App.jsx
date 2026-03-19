428983db8bbb023ecb9623fca5b67a648fb53fed
Ankit <ankitrajdihara123@gmail.com>
2026-03-19 23:18:34 +0530

    root_file&otherscssSTYLING

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import AssignmentList from './pages/AssignmentList/AssignmentList';
 import AssignmentAttempt from './pages/AssignmentAttempt/AssignmentAttempt';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import ManageAssignments from './pages/Admin/ManageAssignments';
+import AssignmentForm from './pages/Admin/AssignmentForm';
 
 function App() {
   return (
@@ -15,6 +18,11 @@ function App() {
           <Route path="/assignment/:id" element={<AssignmentAttempt />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          {/* Admin routes */}
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/assignments" element={<ManageAssignments />} />
+          <Route path="/admin/assignments/new" element={<AssignmentForm />} />
+          <Route path="/admin/assignments/edit/:id" element={<AssignmentForm />} />
         </Routes>
       </main>
     </BrowserRouter>
