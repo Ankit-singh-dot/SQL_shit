8f0778a7eb81d44e1925cdb7f8dcd34b4d3c5509
Ankit <ankitrajdihara123@gmail.com>
2026-03-31 21:43:09 +0530

    updated_attempt

--- backend/models/Attempt.js
+++ backend/models/Attempt.js
@@ -3,9 +3,19 @@ const mongoose = require('mongoose');
 const attemptSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     assignmentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Assignment', required: true },
-    query: { type: String, required: true },
-    result: { type: mongoose.Schema.Types.Mixed },
-    isCorrect: { type: Boolean, default: false },
+    mcqAnswers: [{
+        questionIndex: { type: Number },
+        selectedOptionIndex: { type: Number },
+        isCorrect: { type: Boolean }
+    }],
+    codingAnswers: [{
+        questionIndex: { type: Number },
+        query: { type: String },
+        isCorrect: { type: Boolean }
+    }],
+    score: { type: Number, default: 0 },
+    totalMaxScore: { type: Number, default: 0 },
+    isFullyCorrect: { type: Boolean, default: false },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Attempt', attemptSchema);
