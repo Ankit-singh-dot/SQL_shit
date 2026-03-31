91beb244bb2ad8bd7c0becfe4e8046070a41b305
Ankit <ankitrajdihara123@gmail.com>
2026-03-31 21:43:20 +0530

    user_added_amdin

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, trim: true },
     email: { type: String, required: true, unique: true, trim: true, lowercase: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    role: { type: String, enum: ['user', 'teacher', 'admin'], default: 'user' },
     bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Assignment' }],
 }, { timestamps: true });
 
